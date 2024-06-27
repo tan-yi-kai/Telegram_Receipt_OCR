2ea497e4ab6309cd07a7cd6c6a368ddca5f35e87
tan-yi-kai <tanyikai967@gmail.com>
2024-06-27 22:00:29 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+
+## Function definition
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello ayyyyyy lmao")
+
+
+
+
+
+if __name__ == '__main__':
+    app = Application.builder().token(TOKEN).build()
+    print("Bot started")
+
+
+    ## Commands
+    app.add_handler(CommandHandler('start', start_command))
+
+    ## Bot Polling
+    app.run_polling(poll_interval=3)
+    print("Polling")
+
